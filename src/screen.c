fa99b31074567a61d66752bbf88328e4e14c0a54
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-01-28 19:44:25 +0100

    Screen C functions disable

--- src/screen.c
+++ src/screen.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include "screen.h"
 
-void initScreenContext(ScreenContext* screenContext)
+void initScreenContext(ScreenContext *screenContext)
 {
     screenContext->videoAddress = Logbase();   // Get the logical pointer of the video RAM
     screenContext->savedResolution = Getrez(); // Get current resolution
     savePalette(screenContext->savedPalette);  // Save the palette
-    setFullBlackPalette();                     // put lights off before
+    //    setFullBlackPalette();                     // put lights off before
 }
 
 // saves the current palette into a buffer (works only in supervisor mode)
-void savePalette(__uint16_t* paletteBuffer)
+void savePalette(__uint16_t *paletteBuffer)
 {
-   memcpy(paletteBuffer, PALETTE_ADDRESS, sizeof(__uint16_t) * 16); // 16 colors, 16 bits each.
+    memcpy(paletteBuffer, PALETTE_ADDRESS, sizeof(__uint16_t) * 16); // 16 colors, 16 bits each.
 }
 
 // restores the saved resolution and its palette
-void restoreResolutionAndPalette(ScreenContext* screenContext)
+void restoreResolutionAndPalette(ScreenContext *screenContext)
 {
-    Setscreen((*screenContext).videoAddress,(*screenContext).videoAddress, (*screenContext).savedResolution);
+    Setscreen((*screenContext).videoAddress, (*screenContext).videoAddress, (*screenContext).savedResolution);
     Setpalette((*screenContext).savedPalette);
 }
 
 void setFullBlackPalette()
 {
-    for(int i = 0 ; i < 16 ; i++)
+    for (int i = 0; i < 16; i++)
     {
         Setcolor(i, 0x000);
     }
@@ -32,11 +32,11 @@ void setFullBlackPalette()
 
 void initLowResolution(__uint16_t palette[16])
 {
-    Setscreen(-1,-1, LOW_RES);
+    Setscreen(-1, -1, LOW_RES);
     Setpalette(palette);
 }
 
-void restoreScreenContext(ScreenContext* screenContext)
+void restoreScreenContext(ScreenContext *screenContext)
 {
     restoreResolutionAndPalette(screenContext);
 }
