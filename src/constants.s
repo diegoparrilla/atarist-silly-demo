32d57d9c64326287328df0b706be311e0404b930
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-01-28 19:40:31 +0100

    New global constants file

---
+++ src/constants.s
@@ -0,0 +1,9 @@
+; Constants file
+; all the global constants are defined here
+; They must start with an underscore and a letter
+
+
+_BUFFER_NUMBERS         equ     32          ; number of buffers to use. Only power of 2 allowed (2, 4, 8, 16...)
+_SCREEN_SIZE            equ     32000       ; size of the screen in bytes
+_SCREEN_WIDTH_BYTES     equ     160         ; width of a screen line in bytes
+
