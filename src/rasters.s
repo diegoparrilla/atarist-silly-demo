11a6de8d6a013ece087808f74b3ebf5ed7676029
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-01-28 19:42:57 +0100

    Rasters for the background

--- src/rasters.s
+++ src/rasters.s
@@ -1,25 +1,39 @@
     XDEF    _asm_setup_vblank
     XDEF    _asm_vbl_counter
+    XREF    _asm_palette
+    XREF    _screen_next
 
                 ; Rasters section
 TIMERB_COUNT_EVERY_SCAN_LINE    EQU 1
 TIMERB_EVENT_COUNT              EQU 8
 TILE_COLOR_PALETTE              EQU 32             ; 32 word colors per tile
 COLOR_ITEM_PALETTE_SIZE         EQU 2              ; 2 bytes (1 word) per color
+SCREEN_RASTER_LINES             EQU 192            ; 192 scan lines per screen
+SCREEN_REVERT_RASTER            EQU 64             ; 64 scan lines per screen
 
                 section code
 
 _asm_setup_vblank:
-                clr.w  _asm_vbl_counter         ; clear the vbl counter before starting vblank   
-                clr.w rotate_raster             ; init the rotate raster
+                moveq #0, d0
+                move.w d0,  _asm_vbl_counter         ; clear the vbl counter before starting vblank   
+                move.w d0, rotate_raster             ; init the rotate raster
                 lea vblank_routine, a1          ; get the address of the vblank routine
                 lea timer_b_routine, a2         ; get the address of the timer b routine
                 rts
 
 vblank_routine:
-                addq.w #1,  _asm_vbl_counter                        ; set the vbl counter when the vblank starts   
+                move.l _asm_palette + 0, $ffff8240.w	; Set the palette every vblank
+                move.l _asm_palette + 4, $ffff8244.w
+                move.l _asm_palette + 8, $ffff8248.w
+                move.l _asm_palette + 12, $ffff824c.w
+                move.l _asm_palette + 16, $ffff8250.w
+                move.l _asm_palette + 20, $ffff8254.w
+                move.l _asm_palette + 24, $ffff8258.w
+                move.l _asm_palette + 28, $ffff825c.w
+
+                addq #1, _asm_vbl_counter                           ; set the vbl counter when the vblank starts   
                 clr.w  line_counter                                 ; clear the line counter before starting vblank
-                addq #2, rotate_raster                              ; increment the raster
+                addq #4, rotate_raster                              ; increment the raster
                 and.w #(TILE_COLOR_PALETTE * 2) - 1, rotate_raster
 
                 ;Start up Timer B each VBL
@@ -33,27 +47,32 @@ vblank_routine:
                 move.w	#$2300,sr			                        ;Interrupts back on
                 rte
 
+; Using short reference to the palette with PC relative addressing
 timer_b_routine:
-                movem.l d0-d1/a0, -(a7)
-                move.w line_counter, d0          ; get the current scan line
-                and.w #(TILE_COLOR_PALETTE * 2) - 1, d0                     ; get the current color
-                lea rainbow_colors,a0
-                move.w rotate_raster, d1
-                sub.w d0, d1
-                and.w #(TILE_COLOR_PALETTE * 2) - 1, d1
-                add.w d1, a0                     ; get the next color word
-                move.w (a0), $ff8250             ; set the color
-                addq.w #COLOR_ITEM_PALETTE_SIZE, d0                    ; increment the line counter by a word
-                move d0, line_counter
-                movem.l (a7)+, d0-d1/a0
+                move.w d0, -(a7)
+                move.w line_counter, d0                               ; get the current scan line
+                sub.w rotate_raster, d0
+                and.w #(TILE_COLOR_PALETTE * 2) - 1, d0               ; get the next color word
+                move.w rainbow_colors(pc, d0), $ffff8250.w            ; extract the color from indexed table
+;                cmp.w #(SCREEN_RASTER_LINES - SCREEN_REVERT_RASTER)*2, line_counter
+;                bge.s skip_background_full
+                addq.w #COLOR_ITEM_PALETTE_SIZE, line_counter         ; increment the line counter by a word
+                move.w (a7)+, d0
+                rte
+skip_background_full:
+                move.w $ffff8250.w, d0                                ; get the current color
+                move.w d0, $ffff8252.w                                ; set the color
+                move.w d0, $ffff8254.w                                ; set the color
+                move.w d0, $ffff8256.w                                ; set the color
+                move.w d0, $ffff8258.w                                ; set the color
+                move.w d0, $ffff825a.w                                ; set the color
+                move.w d0, $ffff825c.w                                ; set the color
+                move.w d0, $ffff825e.w                                ; set the color
+                addq.w #COLOR_ITEM_PALETTE_SIZE, line_counter         ; increment the line counter by a word
+                move.w (a7)+, d0
                 rte
 
-                section bss
-line_counter:       ds.w 1
-rotate_raster:      ds.w 1
-_asm_vbl_counter    ds.w 1
-
-                section data align 2
+;                section data align 2
 rainbow_colors: 
                 dc.w $0700     ; red
                 dc.w $0600     ; 
@@ -89,5 +108,8 @@ atari_letters:
                 dc.w $0555     ; white
                 dc.w $0444     ; white
 
-                end
 
+                section bss
+line_counter:       ds.w 1
+rotate_raster:      ds.w 1
+_asm_vbl_counter    ds.w 1
