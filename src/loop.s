5a3c3b73fc6fc95869818fb013fd5c813a1cf04c
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-01-28 19:41:36 +0100

    Main loop with cycling screen buffers

--- src/loop.s
+++ src/loop.s
@@ -1,59 +1,77 @@
+
+                        include src/constants.s     ; Global constants. Start with '_'
+
     XDEF	_asm_main_loop
-    XDEF    _screen_last
+    XDEF    _screen_visible
     XDEF    _screen_next
+    XDEF    _screen_last
     XDEF    _screen_base
-    XDEF    _BUFFER_NUMBERS
+    XDEF    _current_screen_mask
     XREF    _asm_save_state
     XREF    _asm_restore_state
-    XREF    _asm_column_rotate
     XREF    _asm_vbl_counter
     XREF    _asm_print_str
+    XREF    _asm_print_small_str
     XREF    _asm_draw_tiles
     XREF    _asm_setup_vblank
     XREF    _scroll_type
+    XREF	_asm_cook_small_sprites
+    XREF    _asm_restore_all_sprites
+    XREF    _asm_show_all_sprites
+    XREF    _asm_scroll_init
+    XREF    _asm_scroll_rotate
+    XREF    _asm_restore_background_scroll
+
+
+TEXT_INFO_POSITION  equ     184         ; 200 lines - 16 pixels height
+TEXT_INFO_PLANE     equ     0           ; 0 = plane A, 1 = plane B; 2 = plane C, 3 = plane D
 
                 ; Scrolling section
 
                 section code
-_BUFFER_NUMBERS   equ     16          ; number of buffers to use. Only power of 2 allowed (2, 4, 8, 16...)
-SCREEN_SIZE      equ     32000
-
 rotate_screens:
-                move.w current_screen_mask, d0
-                move.l #_screen_base, d1
-                move.l #_screen_base + SCREEN_SIZE, d2
-                move.w d0, d3           ; calculate the screen address of the next buffers
-                tst.w d0
-                beq.s increment_buffer
-                subq #1, d3
-calculate_buffer:
-                add.l #SCREEN_SIZE, d1
-                add.l #SCREEN_SIZE, d2
-                dbf d3, calculate_buffer
-
-increment_buffer:
-                addq.w #1, d0
+                move.w _current_screen_mask, d0
+                addq #1, d0
                 and.w #_BUFFER_NUMBERS-1,d0
-                tst.w d0
-                bne.s save_screen_addr
-                move.l #_screen_base, d2    ; if the buffer restart, use the first buffer for the next screen
-
-save_screen_addr:
-                move.w d0, current_screen_mask
-                clr.b d1    ; put on 256 byte boundary  
-                clr.b d2    ; put on 256 byte boundary  
-                move.l d1, _screen_last
-                move.l d2, _screen_next
-                rts
+                move.w d0, _current_screen_mask
+                add.w d0,d0
+                add.w d0,d0
+                move.w d0,d1
+                addq #4, d1
+                and.w #(_BUFFER_NUMBERS*4)-1,d1
+                lea screen_buffer_index, a0
+                move.l (a0, d0.w), d0
+                move.l (a0, d1.w), d1
+                move.l #_screen_base, a0
+                add.l a0, d0
+                add.l a0, d1
+                clr.b d0                            ; put on 256 byte boundary  
+                clr.b d1                            ; put on 256 byte boundary  
+                move.l d0, _screen_visible             ; IMPORTANT: _screen_visible is the _current_screen_mask value
+                move.l d1, _screen_next             ; IMPORTANT: _screen_next is the next _current_screen_mask value
+                rts                                 
 
 _asm_main_loop:
                 movem.l d0-d7/a0-a6, -(a7)
 
-                bsr.s rotate_screens            ; rotate the screen buffers for the first time
-                move.w #$0, _scroll_type        ; default large text scrolling type
-                bsr print_scroll_8_byte_copy    ; print the scrolling type
+; Initialize the first two buffered screens
+                clr.w _current_screen_mask                          ; set the current screen mask to 0s
+
+                lea _screen_base, a0
+                move.l  #((_SCREEN_SIZE * _BUFFER_NUMBERS)/4)-1, d0 ; Clean all the screens
+                moveq #0, d1                     ; clear with 0
+clean_screen_loop:
+                move.l  d1, (a0)+             ; move one longword to screen
+                dbf     d0, clean_screen_loop
+
+                jsr _asm_cook_small_sprites     ; cook the small sprites
+
+                move.w #$3, _scroll_type        ; default large text scrolling type
+;                bsr print_scroll_8_blitter_copy    ; print the scrolling type
 
                 jsr _asm_draw_tiles             ; draw the tiles on the buffers
+
+                jsr _asm_scroll_init            ; Init scroll variables
 
                 ; The setup_vblank only clear the initial raster and returns
                 ; the addresses of the vblank and timer B (HBL) routines in a1 and a2
@@ -61,38 +79,72 @@ _asm_main_loop:
                 ; ST and the restore_state routine restores it. It expects the 
                 ; addresses of the vblank and timer B routines in a1 and a2 respectively.
                 bsr _asm_setup_vblank
-                bsr _asm_save_state
+                bsr _asm_save_state             ; save the current state of the interrupts and screen
 
-                move.l  _screen_last, d0        ; set the next screen address
+change_screen_buffers:
+; Set the next screen address to be displayed BEFORE THE VERTICAL BLANK HAPPENS
+; IMPORTANT: _screen_next is visible AFTER the vblank interrupt
+                move.l  _screen_next, d0              
                 clr.b   $ffff820d               ; clear STe extra bit  
                 lsr.l   #8, d0    
                 move.b  d0, $ffff8203           ; put in mid screen address byte
                 lsr.w   #8, d0
-                move.b  d0, $ffff8201           ; put in high screen address byte
+                move.b  d0, $ffff8201           ; put in high screen address byte   
 
+; WARNING: the screen_next is visible now. So we need to rotate the screen buffers before drawing anything
+                move.l _screen_visible, _screen_last
+                bsr rotate_screens              ; rotate the screen buffers. YOU SHOULD SAFELY DRAW NOW AFTER THIS POINT
+; after this routine, the screen_next is the next in the buffer and not displayed
+; IMPORTANT: _screen_visible is the _current_screen_mask value
+; IMPORTANT: _screen_next is the next _current_screen_mask value
+; IMPORTANT: _screen_next NOW IS HIDDEN 
+; IMPORTANT: so immediately after the vblank interrupt, the screen should rotate with this routine
 
 main_loop:
+;
+; When entering here, _screen_next should be last hidden buffer. The VBL interrupt will change it to the visible buffer
+;
             	tst.w	_asm_vbl_counter    ; Wait for the VBL
 		        beq.s	main_loop           ; YOU NOT SHALL PASS!!! ...until the VBL changes the state
 		        clr.w	_asm_vbl_counter    ; Clear the VBL counter
 
-                move.l  _screen_next, d0
-              
-                clr.b   $ffff820d               ; clear STe extra bit  
-                lsr.l   #8, d0    
-                move.b  d0, $ffff8203           ; put in mid screen address byte
-                lsr.w   #8, d0
-                move.b  d0, $ffff8201           ; put in high screen address byte
-              
-                move.w  #700, $ff8240
+                tst.w exit_program
+                bne exit
 
-                bsr _asm_column_rotate
+                move.w  #$002, $ff8240
 
-                bsr rotate_screens        ; rotate the screens
+;
+; All the drawing coding should START here
+;
 
+                jsr _asm_restore_all_sprites        ; restore all sprites
+
+                bsr _asm_restore_background_scroll  ; restore the background scroll
+
+                bsr _asm_scroll_rotate              ; rotate the large scroll text
+
+                jsr _asm_show_all_sprites           ; show all sprites
+
+;
+; All the drawing coding should END here
+;
                 move.w  #$000, $ff8240
 
-                ; Test keys
+; Test keys
+                cmp.b #$0A, $fffc02
+                bne.s check_key0
+                cmp.w #$F, skew
+                beq.s check_key0
+                addq #1, skew
+
+check_key0:
+                cmp.b    #$0B, $fffc02            ; Key 0 pressed?
+                bne.b    check_key1               ; Check next key
+                cmp.w #$0, skew
+                beq.s check_key1
+                subq #1, skew                   ; decrease the skew
+
+check_key1:
                 cmp.b    #$02, $fffc02            ; Key 1 pressed?
                 bne.b    check_key2               ; Check next key
                 move.w #$0, _scroll_type
@@ -105,15 +157,18 @@ check_key2:
                 bsr.s print_scroll_8_movep_copy
 
 check_key3:
-                cmp.b    #$04, $fffc02            ; Key 2 pressed?
+                cmp.b    #$04, $fffc02            ; Key 3 pressed?
                 bne.b    check_escape              ; Check next key
                 move.w #$2, _scroll_type
                 bsr.s print_scroll_8_blitter_copy
 
 check_escape:
                 cmp.b    #$01, $fffc02            ; ESC pressed?
-                bne      main_loop                ; if not, repeat main                
+                bne      change_screen_buffers    ; if not, repeat main
+                move.w #1, exit_program
+                bra change_screen_buffers                
 
+exit:
                 bsr _asm_restore_state
 
                 movem.l (a7)+, d0-d7/a0-a6
@@ -145,24 +200,38 @@ print_all_buffers:
                 move.l #_screen_base, d1
                 clr.b d1
                 move.l d1, a3
-                lea (175*160,a3), a3      ; The bottom of the screen
+                lea ((TEXT_INFO_PLANE * 2) + (TEXT_INFO_POSITION * _SCREEN_WIDTH_BYTES),a3), a3      ; The bottom of the screen
 print_next_buffer:
-                bsr _asm_print_str
-                lea (SCREEN_SIZE,a3), a3
+                bsr _asm_print_small_str
+                lea (_SCREEN_SIZE,a3), a3
                 dbf d0, print_next_buffer
                 movem.l (a7)+, d0-d1/a3
 
                 rts
 
-                section bss
+                section bss 
                 ds.b    256
-_screen_base:   ds.b    _BUFFER_NUMBERS * SCREEN_SIZE
+_screen_base:   ds.b    _BUFFER_NUMBERS * _SCREEN_SIZE
 
 
                 section data
-current_screen_mask     dc.w   0
+_current_screen_mask    dc.w   0
 _screen_next            dc.l   0
+_screen_visible         dc.l   0
 _screen_last            dc.l   0
+_sprite_x               dc.w   8
+_sprite_y               dc.w   8
+_sprite_text_x          dc.w   8
+_sprite_text_y          dc.w   8
+skew                    dc.w   15
+exit_program            dc.w   0    
+
+screen_buffer_index     
+                        REPT   _BUFFER_NUMBERS
+                        dc.l   REPTN * _SCREEN_SIZE
+                        ENDR
+    
+
 
 print_scroll_8_byte_str: dc.b   'BYTE COPY',0
 print_scroll_8_movep_str: dc.b  'MOVEP    ',0
