e2a7d2020d7585429ec7f167632885e7db40f8eb
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-02-10 10:51:02 +0100

    large sprites waves python

--- src/small_sprites.py
+++ src/small_sprites.py
@@ -12,7 +12,7 @@ lines_y.append("sprite_y_pos:\n")
 
 # Let's start at the center of the screen witout moving the sprite
 start_position_x = 96
-start_position_y = 80
+start_position_y = 88
 frames = FRAMES_PER_SECOND * 10  # 10 seconds
 for i in range(frames):
     lines_x.append("                dc.w %i\n" % start_position_x)
@@ -29,8 +29,8 @@ for j in range(2):
         lines_x.append("                dc.w %i\n" % int(y))
 
     # Sine wave for Y axis
-    start_position = 80  # The Y axis position 0 is at start_position
-    amplitude = 78  # The amplitude of the cosine wave
+    start_position = 88  # The Y axis position 0 is at start_position
+    amplitude = 88  # The amplitude of the cosine wave
     start_phase = 180  # The phase of the cosine wave to start
     for i in range(entries):
         x = start_phase + (i * 720 / (entries - 1))
@@ -39,7 +39,7 @@ for j in range(2):
 
 # Let's start at the center of the screen witout moving the sprite
 start_position_x = 96
-start_position_y = 80
+start_position_y = 88
 frames = FRAMES_PER_SECOND * 5  # 5 seconds
 for i in range(frames):
     lines_x.append("                dc.w %i\n" % start_position_x)
@@ -56,8 +56,8 @@ for j in range(2):
         lines_x.append("                dc.w %i\n" % int(y))
 
     # Cosine wave for Y axis
-    start_position = 80  # The Y axis position 0 is at start_position
-    amplitude = 78  # The amplitude of the cosine wave
+    start_position = 88  # The Y axis position 0 is at start_position
+    amplitude = 88  # The amplitude of the cosine wave
     start_phase = 90  # The phase of the cosine wave to start
     for i in range(entries):
         x = start_phase + (i * 360 / (entries - 1))
@@ -66,7 +66,7 @@ for j in range(2):
 
 # Let's finish at the center of the screen to hide change in the positions
 start_position_x = 96
-start_position_y = 80
+start_position_y = 88
 frames = FRAMES_PER_SECOND * 3  # 3 seconds
 for i in range(frames):
     lines_x.append("                dc.w %i\n" % start_position_x)
