b25416cc9f2660c12d6de038a08bb8ea2fcaf737
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-03-24 17:05:08 +0100

    Fix the interrupts to support sndh

--- src/init.s
+++ src/init.s
@@ -1,3 +1,5 @@
+                        include src/constants.s     ; Global constants. Start with '_'
+
 ;
 ;       "Inspired" by: https://files.dhs.nu/files_source/spiny.s
 ;
@@ -5,7 +7,7 @@
         XDEF    _asm_restore_state
 		XDEF	_asm_palette
 		XDEF    _asm_palette_red_devil
-
+		XDEF    _asm_palette_dithered
 
         section code
 
@@ -15,9 +17,9 @@
 ;      ; a2: Address of the Timer B routine
 _asm_save_state:
 		lea	save_screenadr,a0		    ;Save old screen address
-		move.b	$ffff8201.w,(a0)+
-		move.b	$ffff8203.w,(a0)+
-		move.b	$ffff820d.w,(a0)+
+		move.b	VIDEO_BASE_ADDR_LOW.w,(a0)+
+		move.b	VIDEO_BASE_ADDR_MID.w,(a0)+
+		move.b	VIDEO_BASE_ADDR_HIGH.w,(a0)+
 
 		movem.l	$ffff8240.w,d0-d7		;Save old palette
 		movem.l	d0-d7,save_palette
@@ -25,8 +27,15 @@ _asm_save_state:
 		move.b	$ffff8260.w,save_resolution		;Save old resolution
 		clr.b	$ffff8260.w			            ;Set low resolution
 
-		movem.l _asm_palette, d0-d7			; Set new palette
+		movem.l _asm_palette_dithered, d0-d7			; Set new palette
 		movem.l d0-d7, $ffff8240.w
+
+
+;		move.w	#$2700,sr			    ;Stop all interrupts
+;		move.l	$70.w,save_vbl			;Save old VBL
+;		move.l	a1,$70.w			    ;Install our own VBL
+;		move.w	#$2300,sr			;Interrupts back on
+;		rts 
 
 		move.w	#$2700,sr			    ;Stop all interrupts
 		move.l	$70.w,save_vbl			;Save old VBL
@@ -43,12 +52,15 @@ _asm_save_state:
 		move.l	#dummy,$114.w			;Install our own Timer C (dummy)
 		move.l	#dummy,$110.w			;Install our own Timer D (dummy)
 		move.l	#dummy,$118.w			;Install our own ACIA (dummy)
+		move.b  $fffffa07.w,save_inta		;Save MFP state for interrupt enable A
+		move.b  $fffffa13.w,save_inta_mask	;Save MFP state for interrupt mask A
 		move.b	$fffffa09.w,save_intb		;Save MFP state for interrupt enable B
 		move.b	$fffffa15.w,save_intb_mask	;Save MFP state for interrupt mask B
-		clr.b	$fffffa07.w			;Interrupt enable A (Timer-A & B)
-		clr.b	$fffffa13.w			;Interrupt mask A (Timer-A & B)
-		clr.b	$fffffa09.w			;Interrupt enable B (Timer-C & D)
-		clr.b	$fffffa15.w			;Interrupt mask B (Timer-C & D)
+;		clr.b	$fffffa07.w			;Interrupt enable A (Timer-A & B)
+;        move.b #-1, $fffffa07.w
+;		clr.b	$fffffa13.w			;Interrupt mask A (Timer-A & B)
+;		clr.b	$fffffa09.w			;Interrupt enable B (Timer-C & D)
+;		clr.b	$fffffa15.w			;Interrupt mask B (Timer-C & D)
 		move.w	#$2300,sr			;Interrupts back on
 
 		move.b	#$12,$fffffc02.w		;Kill mouse
@@ -57,8 +69,8 @@ _asm_save_state:
 
 _asm_restore_state:
 		move.w	#$2700,sr			;Stop all interrupts
-		clr.b	$fffffa1b.w			;Timer B control (Stop)
 		move.l	save_vbl,$70.w			;Restore old VBL
+		clr.b	$fffffa1b.w			;Timer B control (Stop)
 		move.l	save_hbl,$68.w			;Restore old HBL
 		move.l	save_ta,$134.w			;Restore old Timer A
 		move.l	save_tb,$120.w			;Restore old Timer B
@@ -67,6 +79,8 @@ _asm_restore_state:
 		move.l	save_acia,$118.w		;Restore old ACIA
 		move.b	save_intb,$fffffa09.w		;Restore MFP state for interrupt enable B
 		move.b	save_intb_mask,$fffffa15.w	;Restore MFP state for interrupt mask B
+		move.b	save_inta,$fffffa07.w		;Restore MFP state for interrupt enable A
+		move.b	save_inta_mask,$fffffa13.w	;Restore MFP state for interrupt mask A
 		move.w	#$2300,sr			;Interrupts back on
 
 		move.b	save_resolution,$ffff8260.w		;Restore old resolution
@@ -75,9 +89,13 @@ _asm_restore_state:
 		movem.l	d0-d7,$ffff8240.w
 
 		lea	save_screenadr,a0		;Restore old screen address
-		move.b	(a0)+,$ffff8201.w
-		move.b	(a0)+,$ffff8203.w
-		move.b	(a0)+,$ffff820d.w
+		move.b	(a0)+,VIDEO_BASE_ADDR_LOW.w
+		move.b	(a0)+,VIDEO_BASE_ADDR_MID.w
+		move.b	(a0)+,VIDEO_BASE_ADDR_HIGH.w
+
+		moveq #0,d0
+		move.b d0, VIDEO_BASE_PIXEL_OFFSET.w	; Restore the pixel offset to 0
+		move.b d0, VIDEO_BASE_LINE_OFFSET.w		; Restore the line offset to 0
 
 		move.b	#$8,$fffffc02.w			;Enable mouse
 		rts
@@ -94,15 +112,22 @@ save_tb:	        ds.l	1
 save_tc:	        ds.l	1
 save_td:	        ds.l	1
 save_acia:	        ds.l	1
+save_inta:	        ds.b	1
+save_inta_mask:	    ds.b	1
 save_intb:	        ds.b	1
 save_intb_mask:	    ds.b	1
 save_resolution:	ds.b	1
             		even
 
 		section	data align 2
-_asm_palette:        		dc.w $000,$756,$645,$534,$423,$312,$201,$777,$222,$756,$645,$534,$423,$312,$201,$777
+_asm_palette:        		dc.w $000,$756,$645,$534,$423,$312,$201,$777,$444,$756,$645,$534,$423,$312,$201,$777
 
 ; RED DEVIL PALETTE
-_asm_palette_red_devil:     dc.w $000,$657,$546,$435,$324,$213,$102,$777,$222,$657,$546,$435,$324,$213,$102,$777
+_asm_palette_red_devil:     dc.w $000,$657,$546,$435,$324,$213,$102,$777,$444,$657,$546,$435,$324,$213,$102,$777
 
+; DITHERED PALETTE
+;_asm_palette_dithered: 		dc.w $000,$C05,$555,$FCE,$00F,$A89,$5AB,$2AE,$DDD,$C05,$555,$FCE,$00F,$A89,$5AB,$2AE
+;_asm_palette_dithered: 		dc.w $000,$FC6,$393,$901,$CAC,$282,$6BD,$FFF,$DDD,$FC6,$393,$901,$CAC,$282,$6BD,$FFF
+;_asm_palette_dithered: 		dc.w $000,$F9C,$A58,$636,$75A,$A7C,$303,$414,$DDD,$F9C,$A58,$636,$75A,$A7C,$303,$414
+_asm_palette_dithered: 		dc.w $000,$E4D,$393,$901,$5A4,$BA5,$289,$FFF,$DDD,$E4D,$393,$901,$5A4,$BA5,$289,$FFF
 		end
